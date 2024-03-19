ae4035198f212ff9ccb01fbd420ee9f67f907832
poyen0806 <leo33917888@gmail.com>
2024-03-19 16:25:28 +0800

    refactor: recode the main with platform_view_model

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart' hide Router;
 import 'package:flutter_web/firebase_options.dart';
-import 'package:flutter_web/view/page/computer_layout.dart';
-import 'package:flutter_web/view/page/mobile_layout.dart';
-import 'package:flutter_web/view/page/tablet_layout.dart';
+import 'package:flutter_web/view_model/platform_view_model.dart';
 import 'package:flutter_web/view_model/theme_view_model.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:flutter_web/router/router.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
 Future<void> main() async {
   setPathUrlStrategy();
@@ -20,6 +17,7 @@ Future<void> main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ThemeViewModel()),
+        ChangeNotifierProvider(create: (_) => PlatformViewModel()),
       ],
       child: const Main(),
     ),
@@ -31,24 +29,13 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeViewModel>(
-      builder: (context, themeVM, child) => MaterialApp.router(
+    return Consumer2<ThemeViewModel, PlatformViewModel>(
+      builder: (context, themeVM, pfVM, child) => MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: "Poyen",
         theme: themeVM.themeData,
         locale: const Locale("zh", "TW"),
         routerConfig: Router.router,
-        builder: (context, navigator) => ResponsiveBuilder(
-          builder: (context, sizingInformation) {
-            if (sizingInformation.deviceScreenType == DeviceScreenType.desktop) {
-              return const ComputerLayout();
-            } else if (sizingInformation.deviceScreenType == DeviceScreenType.tablet) {
-              return const TabletLayout();
-            } else {
-              return const MobileLayout();
-            }
-          },
-        ),
       ),
     );
   }
